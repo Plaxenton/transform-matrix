97724e3fd435351dcf04feccc76496afcde8a715
Plaxenton <codecorsair@foxmail.com>
2024-02-28 21:52:16 +0800

    添加正文

--- transform matrix/main.c
+++ transform matrix/main.c
@@ -2,6 +2,32 @@
 
 int main(void)
 {
-
+	int matrix[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+	for (int line = 0; line < 3; line++)
+	{
+		for (int column = 0; column < 3; column++)
+		{
+			printf("%d ", matrix[line][column]);
+		}
+		printf("\n");
+	}
+	for (int line = 0; line < 3; line++)
+	{
+		for (int column = 0; column < line; column++)
+		{
+			int temp = matrix[line][column];
+			matrix[line][column] = matrix[column][line];
+			matrix[column][line] = temp;
+		}
+	}
+	printf("\n");
+	for (int line = 0; line < 3; line++)
+	{
+		for (int column = 0; column < 3; column++)
+		{
+			printf("%d ", matrix[line][column]);
+		}
+		printf("\n");
+	}
 	return 0;
 }
